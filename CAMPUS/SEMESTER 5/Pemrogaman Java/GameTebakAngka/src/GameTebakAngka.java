fbcfe383a6ef46eb0211d3d76da1002766b9a682
rinaseptiana <rina.eresty@gmail.com>
2016-11-21 23:46:00 +0700

    <12141380> Membuat Class Game Tebak Angka serta Script ini berfungsi unuk menginputkan nama pemain yang akan memainkan gamenya

---
+++ CAMPUS/SEMESTER 5/Pemrogaman Java/GameTebakAngka/src/GameTebakAngka.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class GameTebakAngka {
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Masukkan Nama Anda: ");
+            String nama = input.nextLine();
+        
+       
+
+
+    
+    
