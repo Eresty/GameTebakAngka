17cb1a736c739e7931006551e8bfe995108e53e5
rinaseptiana <rina.eresty@gmail.com>
2016-11-22 21:59:08 +0700

    <12141380>Menampilkan package yang sudah kita setting ulang, dalam package ini tampilannya yaitu com.RinaEresty.app12141380 <12141380>Membuat Array nama pemain serta skor yang didapat oleh pemain. Match random berfungsi untuk mengacak serta memilih angka acak yang sudah diinputkan

--- CAMPUS/SEMESTER 5/Pemrogaman Java/GameTebakAngka/src/com/RinaEresty/app12141380/GameTebakAngka.java
+++ CAMPUS/SEMESTER 5/Pemrogaman Java/GameTebakAngka/src/com/RinaEresty/app12141380/GameTebakAngka.java
@@ -13,5 +13,19 @@ import java.util.Scanner;
  * 12141380
  * @author Rina Septiana
  */
-
+public class GameTebakAngka {
+    public static void main(String[] args) {
+        ArrayList <String> namaPemain = new ArrayList<>();
+        ArrayList <Integer> sekorPermainan = new ArrayList<>(); 
+        Scanner input = new Scanner(System.in); 
+        Scanner input2= new Scanner(System.in); 
+        Random angkaRandom=new Random(); 
+        String cariY; 
+        int a = (int)(Math.random() *100);
+        int c = 4;
+            System.out.println("===============================================");
+            System.out.println("MARI MENEBAK ANGKA 1 - 100 DENGAN 5X KESEMPATAN");
+            System.out.println("===============================================");
+        
+    
 
